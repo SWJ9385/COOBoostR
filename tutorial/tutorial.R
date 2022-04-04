09f1106f86b21a337880e3c645063a0303bb28fa
WOOJEUNG SONG <tptt1212@gmail.com>
2022-04-04 19:25:01 +0900

    Create tutorial.R

---
+++ tutorial/tutorial.R
@@ -0,0 +1,11 @@
+rm(list=ls())
+
+packagePath <- "path/to/COOBoostRpackage/"
+
+devtools::install(packagePath)
+
+
+sourcePath <- "path/to/tutorial_data/"
+resultsPath <- "path/to/results/" 
+epimarker <- "COO_epimarker.csv"
+mutation <- "COO_mutation.csv"
