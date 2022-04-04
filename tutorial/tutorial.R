56b688b0f60883a22e002f8796d13aeac5ffb54e
WOOJEUNG SONG <tptt1212@gmail.com>
2022-04-04 19:51:49 +0900

    Update tutorial.R

--- tutorial/tutorial.R
+++ tutorial/tutorial.R
@@ -9,3 +9,5 @@ sourcePath <- "path/to/tutorial_data/"
 resultsPath <- "path/to/results/" 
 epimarker <- "COO_epimarker.csv"
 mutation <- "COO_mutation.csv"
+
+COOBoostR(sourcePath = sourcePath, resultPath = resultPath ,epimarker_rawdata = epimarker_rawdata , mutation_rawdata = mutation_rawdata, mEta = 0.01, mdepth = 2)
