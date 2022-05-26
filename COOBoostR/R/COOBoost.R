6024c611a9f1c855709817b3437279db6d58631b
WOOJEUNG SONG <tptt1212@gmail.com>
2022-05-26 14:53:16 +0900

    Update COOBoost.R

--- COOBoostR/R/COOBoost.R
+++ COOBoostR/R/COOBoost.R
@@ -4,7 +4,7 @@
 #' @name COOBoostR
 #' @import xgboost
 #' @import stringr
-#' @importFrom utils capture.output read.csv
+#' @importFrom utils capture.output read.csv read.table write.csv write.table
 #' @description An extreme gradient boosting-based tool for robust tissue or cell-of-origin prediction of tumors
 NULL
 ## NULL
